7c66dcc2191bfec704513cbad6e6a16bb3cb0a50
hemant0621 <hemant.kumar.2335.h@gmail.com>
2024-07-09 11:19:46 +0530

    edit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,7 @@ import Dashboard from "@/components/Dashboard";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { darkModeState } from "@/hooks/darkmode";
+import { useEffect } from "react";
 import { RecoilRoot, useRecoilState } from 'recoil';
 
 export default function Home() {
@@ -19,22 +20,21 @@ const MainApp = () => {
 
   const [dark, setDark] = useRecoilState(darkModeState);
 
-  if (typeof window !== 'undefined'){
-    if(!localStorage.getItem('token')){
-      location.href = '/signup'
-      return <></>
+  useEffect(() => {
+    if (typeof window !== 'undefined'){
+      console.log(window.localStorage.getItem('token'))
     }
-  }
-  else{
-    return (
-      <div className={`grid grid-cols-12 w-full h-screen overflow-y-hidden font-Clash ${dark ? "dark" : ""} transition-colors duration-400 ease-linear bg-[#f3aa4e] dark:bg-[#111820] dark:text-white `}>
-        <Sidebar />
-        <div className="col-span-10 h-screen flex flex-col justify-between">
-          <Navbar />
-          <Dashboard />
-        </div>
+  },[])
+
+  return (
+    <div className={`grid grid-cols-12 w-full h-screen overflow-y-hidden font-Clash ${dark ? "dark" : ""} transition-colors duration-400 ease-linear bg-[#f3aa4e] dark:bg-[#111820] dark:text-white `}>
+      <Sidebar />
+      <div className="col-span-10 h-screen flex flex-col justify-between">
+        <Navbar />
+        <Dashboard />
       </div>
-    );
-  }
+    </div>
+  );
+
 
 };
