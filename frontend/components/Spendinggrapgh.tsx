8bfcbfb2dcb2c45011364e4674cfd94f770170a2
hemant0621 <hemant.kumar.2335.h@gmail.com>
2024-08-16 13:09:01 +0530

    edit

--- frontend/components/Spendinggrapgh.tsx
+++ frontend/components/Spendinggrapgh.tsx
@@ -46,8 +46,8 @@ function Spendinggraph() {
     }
 
     return (
-        <div className='w-full flex flex-col md:flex-row gap-5 md:gap-10 mt-8 md:px-5 h-full'>
-            <div className='relative w-full flex justify-center pt-3 md:w-7/12 h-[40rem] md:h-full bg-white dark:bg-[#353148] rounded-xl md:rounded-2xl border border-black'>
+        <div className='w-full flex flex-col md:flex-row gap-5 md:gap-10 mt-8'>
+            <div className='relative w-full flex justify-center pt-3 md:w-7/12 h-[40rem] md:h-[58vh] bg-white dark:bg-[#353148] rounded-xl md:rounded-2xl border border-black'>
                 <div ref={chartRef} className="w-full h-full pl-2 " ></div>
                 <button className='absolute top-3 right-2 md:right-6 group/main text-end text-sm md:text-base lg:text-lg font-medium text-red-700 md:text-slate-400 p-1 cursor-pointer'>
                     {name}
@@ -71,9 +71,9 @@ function Spendinggraph() {
                     </div>
                 </button>
             </div>
-            <div className=' w-full md:w-5/12 bg-white dark:bg-[#353148] rounded-xl md:rounded-2xl h-full border border-black'>
+            <div className=' w-full md:w-5/12 bg-white dark:bg-[#353148] rounded-xl md:rounded-2xl border border-black md:h-[58vh]'>
                 <h1 className='w-full text-center font-bold text-base md:text-lg lg:text-2xl pt-2'>Last 10 Purchases</h1>
-                <div className='w-full h-full flex flex-col gap-3 px-1 md:px-5  scrollbar scrollbar-track-rounded-lg scrollbar-thumb-rounded-lg scrollbar-thumb-black dark:scrollbar-thumb-white scrollbar-track-[#f3aa4e] dark:scrollbar-track-[#111820] '>
+                <div className='w-full h-[90%] flex flex-col gap-3 px-1 md:px-5  scrollbar scrollbar-track-rounded-lg scrollbar-thumb-rounded-lg scrollbar-thumb-black dark:scrollbar-thumb-white scrollbar-track-[#f3aa4e] dark:scrollbar-track-[#111820] '>
                     <div className=' px-2 md:px-8 py-2 flex w-full justify-between border-4 border-white dark:border-[#353148] border-b-[#f3aa4e] dark:border-b-[#111820] '>
                         <h1 className=' w-1/4 font-medium font-Clash text-base md:text-lg text-left '>Item</h1>
                         <h1 className=' w-1/4 font-medium font-Clash text-base md:text-lg text-center '>Category</h1>
@@ -82,7 +82,7 @@ function Spendinggraph() {
                     </div>
 
 
-                    <div className=' px-2 md:px-5 scrollbar-thin h-[75%] py-1 overflow-y-auto flex flex-col gap-2 '>
+                    <div className=' px-2 md:px-5 scrollbar-thin py-1 overflow-y-auto flex flex-col gap-2 '>
 
                         {past.length>0?past.map((item:{
                             heading : string,
