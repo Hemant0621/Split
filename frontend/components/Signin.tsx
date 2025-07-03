7790fe5c67b6fc8f3e89bc95aa8e137b05bc467a
Hemant0621 <hemant.kumar.2335.h@gmail.com>
2025-07-03 13:11:17 +0530

    edit signup and signin pages

--- frontend/components/Signin.tsx
+++ frontend/components/Signin.tsx
@@ -1,51 +1,96 @@
-'use client'
-import React, { useState } from 'react'
-import axios from 'axios'
-import { DATABASE_URL } from '@/config'
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import { DATABASE_URL } from "@/config";
 
 function Signincomponent() {
-    const [Email, setEmail] = useState('')
-    const [password, setpassword] = useState('')
+  const [Email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
 
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-center">Sign In</h2>
-                <form>
-                    <div className="mb-4">
-                        <label htmlFor="Email" className="block text-gray-700 font-bold mb-2">Email</label>
-                        <input type="email" id="Email" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e) => { setEmail(e.target.value) }} />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-                        <input type="password" id="password" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e) => { setpassword(e.target.value) }} />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600" onClick={async () => {
-
-                            const response = await axios.post(`${DATABASE_URL}/user/signin`, {
-                                Email,
-                                password
-                            }, {
-                                headers: {
-                                    "Access-Control-Allow-Origin": "*",
-                                    "Content-Type": "application/json",
-                                }
-                            }).catch((error)=>{
-                                alert(error.response.data.message)
-                            }).then((response)=>{
-                                if(response?.data.token){
-                                    localStorage.setItem('token', response?.data.token)
-                                    location.href='/'
-                                }
-                            })
-                        }}>Sign In</button>
-                        <a href="/signup" className="text-blue-500 hover:underline">Register for new user?</a>
-                    </div>
-                </form>
+  return (
+    <div className="max-h-screen max-w-screen flex ">
+      <div className="bg-white rounded-r-xl h-screen shadow-lg w-2/5 flex justify-center items-center">
+        <div className="w-4/5 ">
+          <h2 className="text-2xl font-bold mb-6 ">
+            Welcome, Sign In to continue
+          </h2>
+          <form>
+            <div className="mb-4">
+              <label
+                htmlFor="Email"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="Email"
+                className="w-full px-4 py-2 border-2 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full px-4 py-2 border-2 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+                onChange={(e) => {
+                  setpassword(e.target.value);
+                }}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="button"
+                className="bg-[#f3aa4e] text-white px-4 py-2 rounded-lg hover:bg-[#eb9021]"
+                onClick={async () => {
+                  const response = await axios
+                    .post(
+                      `${DATABASE_URL}/user/signin`,
+                      {
+                        Email,
+                        password,
+                      },
+                      {
+                        headers: {
+                          "Access-Control-Allow-Origin": "*",
+                          "Content-Type": "application/json",
+                        },
+                      }
+                    )
+                    .catch((error) => {
+                      alert(error.response.data.message);
+                    })
+                    .then((response) => {
+                      if (response?.data.token) {
+                        localStorage.setItem("token", response?.data.token);
+                        location.href = "/";
+                      }
+                    });
+                }}
+              >
+                Sign In
+              </button>
+              <a href="/signup" className="text-blue-500 hover:underline">
+                Register for new user?
+              </a>
+            </div>
+          </form>
         </div>
-    )
+      </div>
+      <img className="w-3/5" src="/sign.gif" alt="" />
+    </div>
+  );
 }
 
-export default Signincomponent
+export default Signincomponent;
--- frontend/components/Signup.tsx
+++ frontend/components/Signup.tsx
@@ -4,7 +4,6 @@ import axios from 'axios'
 import { DATABASE_URL } from '@/config'
 
 function Signupcomponent() {
-
     const [formData, setFormData] = useState({
         Email: '',
         firstName: '',
@@ -29,86 +28,121 @@ function Signupcomponent() {
             username: string,
             password: string
         }> = {};
-        if (!formData.Email) formErrors.Email = 'Title is required';
-        if (!formData.firstName) formErrors.firstName = 'Description is required';
-        if (!formData.lastName) formErrors.lastName = 'Description is required';
-        if (!formData.username) formErrors.username = 'Time is required';
-        if (!formData.password) formErrors.password = 'Location is required';
+        if (!formData.Email) formErrors.Email = 'Email is required';
+        if (!formData.firstName) formErrors.firstName = 'First name is required';
+        if (!formData.lastName) formErrors.lastName = 'Last name is required';
+        if (!formData.username) formErrors.username = 'Username is required';
+        if (!formData.password) formErrors.password = 'Password is required';
 
         setErrors(formErrors);
         return Object.keys(formErrors).length === 0;
     };
 
-
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-                <form>
-                    <div className="mb-4">
-                        <label htmlFor="Email" className="block text-gray-700 font-bold mb-2">Email</label>
-                        <input type="email" id="Email" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e) => { setFormData({ ...formData, Email: e.target.value }) }} />
-                        {errors.Email && <p className='text-red-600 my-1'>*Email Field is required</p>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
-                        <input type="text" id="Username" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e) => { setFormData({ ...formData, username: e.target.value }) }} />
-                        {errors.username && <p className='text-red-600 my-1'>*Username Field is required</p>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="firstName" className="block text-gray-700 font-bold mb-2">firstName</label>
-                        <input type="text" id="firstName" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e) => { setFormData({ ...formData, firstName: e.target.value }) }} />
-                        {errors.firstName && <p className='text-red-600 my-1'>*firstname Field is required</p>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="lastName" className="block text-gray-700 font-bold mb-2">lastName</label>
-                        <input type="text" id="lastName" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e) => { setFormData({ ...formData, lastName: e.target.value }) }} />
-                        {errors.lastName && <p className='text-red-600 my-1'>*Lastname Field is required</p>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-                        <input type="password" id="password" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required onChange={(e) => { setFormData({ ...formData, password: e.target.value }) }} />
-                        {errors.password && <p className='text-red-600 my-1'>*Password Field is required</p>}
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button type='button' className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600" onClick={async () => {
-                            if (!validateForm()) {
-                                alert("Please fill in all required fields");
-                                return;
-                            }
-                            else {
-                                const response = await axios.post(`${DATABASE_URL}/user/signup`,
-                                    {
-                                        Email: formData.Email,
-                                        username: formData.username,
-                                        firstName: formData.firstName,
-                                        lastName: formData.lastName,
-                                        password: formData.password
-                                    }, {
-                                    headers: {
-                                        "Access-Control-Allow-Origin": "*",
-                                        "Content-Type": "application/json",
+        <div className="max-h-screen max-w-screen flex">
+            <div className="bg-white rounded-r-xl h-screen shadow-lg w-2/5 flex justify-center items-center">
+                <div className="w-4/5">
+                    <h2 className="text-2xl font-bold mb-6">
+                        Welcome, Sign Up to get started
+                    </h2>
+                    <form>
+                        <div className="mb-4">
+                            <label htmlFor="Email" className="block text-gray-700 font-bold mb-2">Email</label>
+                            <input 
+                                type="email" 
+                                id="Email" 
+                                className="w-full px-4 py-2 border-2 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                required 
+                                onChange={(e) => { setFormData({ ...formData, Email: e.target.value }) }} 
+                            />
+                            {errors.Email && <p className='text-red-600 my-1'>*Email Field is required</p>}
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
+                            <input 
+                                type="text" 
+                                id="username" 
+                                className="w-full px-4 py-2 border-2 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                required 
+                                onChange={(e) => { setFormData({ ...formData, username: e.target.value }) }} 
+                            />
+                            {errors.username && <p className='text-red-600 my-1'>*Username Field is required</p>}
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="firstName" className="block text-gray-700 font-bold mb-2">First Name</label>
+                            <input 
+                                type="text" 
+                                id="firstName" 
+                                className="w-full px-4 py-2 border-2 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                required 
+                                onChange={(e) => { setFormData({ ...formData, firstName: e.target.value }) }} 
+                            />
+                            {errors.firstName && <p className='text-red-600 my-1'>*First name Field is required</p>}
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="lastName" className="block text-gray-700 font-bold mb-2">Last Name</label>
+                            <input 
+                                type="text" 
+                                id="lastName" 
+                                className="w-full px-4 py-2 border-2 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                required 
+                                onChange={(e) => { setFormData({ ...formData, lastName: e.target.value }) }} 
+                            />
+                            {errors.lastName && <p className='text-red-600 my-1'>*Last name Field is required</p>}
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
+                            <input 
+                                type="password" 
+                                id="password" 
+                                className="w-full px-4 py-2 border-2 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                required 
+                                onChange={(e) => { setFormData({ ...formData, password: e.target.value }) }} 
+                            />
+                            {errors.password && <p className='text-red-600 my-1'>*Password Field is required</p>}
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <button 
+                                type='button' 
+                                className="bg-[#f3aa4e] text-white px-4 py-2 rounded-lg hover:bg-[#eb9021]" 
+                                onClick={async () => {
+                                    if (!validateForm()) {
+                                        alert("Please fill in all required fields");
+                                        return;
                                     }
-                                }).catch((error) => {
-                                    alert(error.response.data.message)
-                                }).then((response)=>{
-                                    if(response?.data.token){
-                                        localStorage.setItem('token', response?.data.token)
-                                        location.href = '/'
+                                    else {
+                                        const response = await axios.post(`${DATABASE_URL}/user/signup`,
+                                            {
+                                                Email: formData.Email,
+                                                username: formData.username,
+                                                firstName: formData.firstName,
+                                                lastName: formData.lastName,
+                                                password: formData.password
+                                            }, {
+                                            headers: {
+                                                "Access-Control-Allow-Origin": "*",
+                                                "Content-Type": "application/json",
+                                            }
+                                        }).catch((error) => {
+                                            alert(error.response.data.message)
+                                        }).then((response)=>{
+                                            if(response?.data.token){
+                                                localStorage.setItem('token', response?.data.token)
+                                                location.href = '/'
+                                            }
+                                        })
                                     }
-                                })
-
-
-
-                            }
-
-                        }}>Sign Up</button>
-                        <a href="/signin" className="text-blue-500 hover:underline" >Already have an account?</a>
-                    </div>
-                </form>
+                                }}
+                            >
+                                Sign Up
+                            </button>
+                            <a href="/signin" className="text-blue-500 hover:underline">Already have an account?</a>
+                        </div>
+                    </form>
+                </div>
             </div>
+            <img className="w-3/5" src="/sign.gif" alt="" />
         </div>
-
     )
 }
 
